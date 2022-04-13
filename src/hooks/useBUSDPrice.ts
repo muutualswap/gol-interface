89aa2ed592e9effa9217e71cf0976fd198fdfd69
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-12 22:22:46 -0400

    Update useBUSDPrice.ts

--- src/hooks/useBUSDPrice.ts
+++ src/hooks/useBUSDPrice.ts
@@ -1,4 +1,4 @@
-import { ChainId, Currency, currencyEquals, JSBI, Price } from 'golswap-sdk'
+import { ChainId, Currency, currencyEquals, JSBI, Price } from '@bigswap/sdk'
 import { useMemo } from 'react'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import tokens, { mainnetTokens } from 'config/constants/tokens'
