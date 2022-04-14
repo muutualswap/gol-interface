73e992117268cdb52ed836282edffc7cd3c92f7b
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-13 22:55:11 -0400

    Update FormattedPriceImpact.tsx

--- src/views/Swap/components/FormattedPriceImpact.tsx
+++ src/views/Swap/components/FormattedPriceImpact.tsx
@@ -1,4 +1,4 @@
-import { Percent } from 'golswap-sdk'
+import { Percent } from '@bigswap/sdk'
 import React from 'react'
 import { warningSeverity } from 'utils/prices'
 import { ONE_BIPS } from '../../../config/constants'
