8bbc4c1e3f22f68ba6542c9739fc11d16e5bde20
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-13 20:10:53 -0400

    Update hooks.ts

--- src/state/wallet/hooks.ts
+++ src/state/wallet/hooks.ts
@@ -1,4 +1,4 @@
-import { Currency, CurrencyAmount, ETHER, JSBI, Token, TokenAmount } from 'golswap-sdk'
+import { Currency, CurrencyAmount, ETHER, JSBI, Token, TokenAmount } from '@bigswap/sdk'
 import { useMemo } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import ERC20_INTERFACE from 'config/abi/erc20'
