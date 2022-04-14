01f4eaedeff04ec22d6edf8920ae397aca51d7c8
muutualswap <75495839+muutualswap@users.noreply.github.com>
2022-04-13 20:10:09 -0400

    Update hooks.ts

--- src/state/burn/hooks.ts
+++ src/state/burn/hooks.ts
@@ -1,4 +1,4 @@
-import { Currency, CurrencyAmount, JSBI, Pair, Percent, TokenAmount } from 'golswap-sdk'
+import { Currency, CurrencyAmount, JSBI, Pair, Percent, TokenAmount } from '@bigswap/sdk'
 import { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
